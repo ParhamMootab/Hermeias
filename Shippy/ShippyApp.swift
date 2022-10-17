318ed37bb1d095962bce6905022dd3c7b4d7adf7
Parham Mootab <psm1379@gmail.com>
2022-10-16 17:20:46 -0700

    Initial Commit

---
+++ Shippy/ShippyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShippyApp.swift
+//  Shippy
+//
+//  Created by Parham Shagerdmootab on 2022-10-16.
+//
+
+import SwiftUI
+
+@main
+struct ShippyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
